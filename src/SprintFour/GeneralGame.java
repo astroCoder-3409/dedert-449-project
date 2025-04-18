f2ad445617d8af1922cfdd44c5a4541591a4e5e6
RJ Dedert <rdeed@kc.rr.com>
2025-04-17 23:28:04 -0500

    sprint four progress, add chatgpt integration

---
+++ src/SprintFour/GeneralGame.java
@@ -0,0 +1,75 @@
+package SprintFour;
+
+import java.util.ArrayList;
+
+public class GeneralGame extends AbstractGame {
+    private int redScore;
+    private int blueScore;
+
+    public GeneralGame(int _boardSize) {
+        boardSize = _boardSize;
+        grid = new int[boardSize][boardSize];
+        SOSList = new ArrayList<>();
+        gameMode = 2;
+        filledSquareCount = 0;
+        gameState = GameState.ACTIVE;
+        redScore = 0;
+        blueScore = 0;
+    }
+
+    @Override
+    public boolean isGameOver(int row, int column) {
+        boolean gameOver = filledSquareCount == boardSize * boardSize;
+        if (gameOver) {
+            int blueCount = 0;
+            int redCount = 0;
+            for (SOSRecord sosRecord : SOSList) {
+                if (sosRecord.color == "Blue") {
+                    blueCount++;
+                } else {
+                    redCount++;
+                }
+            }
+            if (blueCount > redCount) {
+                gameState = GameState.BLUE_WON;
+            } else if (blueCount == redCount) {
+                gameState = GameState.DRAW;
+            } else {
+                gameState = GameState.RED_WON;
+            }
+        }
+        return gameOver;
+    }
+
+    @Override
+    public void incrementScore(String color) {
+        if(color == "Blue") {
+            blueScore++;
+        } else {
+            redScore++;
+        }
+    }
+
+    @Override
+    public boolean makeMove(int row, int column, boolean isS) {
+        boolean success = super.makeMove(row, column, isS);
+        if (success) {
+            boolean areThereNewSOSs = findNewSOSs(row, column);
+            if (areThereNewSOSs) {
+                isGameOver(row, column);
+            } else {
+                isGameOver(row, column);
+                turn = (turn == "Red") ? "Blue" : "Red";
+            }
+        }
+        return success;
+    }
+
+    public int getBlueScore() {
+        return blueScore;
+    }
+
+    public int getRedScore() {
+        return redScore;
+    }
+}
---
+++ src/SprintFour/SOSRecord.java
@@ -0,0 +1,33 @@
+package SprintFour;
+
+import java.util.Arrays;
+
+public class SOSRecord {
+    public int[] coordinateStart;
+    public int[] coordinateEnd;
+    public String color;
+    public SOSRecord(int[] coordinateStart, int[] coordinateEnd, String color) {
+        this.coordinateStart = coordinateStart;
+        this.coordinateEnd = coordinateEnd;
+        this.color = color;
+    }
+
+    //Source of idea: https://stackoverflow.com/questions/16069106/how-to-compare-two-java-objects
+    @Override
+    public boolean equals(Object other) {
+        if (!(other instanceof SOSRecord)) {
+            return false;
+        }
+
+        SOSRecord that = (SOSRecord) other;
+
+        System.out.println("start: " + this.coordinateStart[0] + ", " + this.coordinateStart[1] + " ||| " + that.coordinateStart[0] + ", " + that.coordinateStart[1]);
+        System.out.println("end: " + this.coordinateEnd[0] + ", " + this.coordinateEnd[1] + " ||| " + that.coordinateEnd[0] + ", " + that.coordinateEnd[1] + "\n");
+
+        // We don't really need to check the other fields, just checking if an SOS already exists with the same coordinates.
+        return (Arrays.equals(this.coordinateStart, that.coordinateStart)
+                && Arrays.equals(this.coordinateEnd, that.coordinateEnd))
+                || (Arrays.equals(this.coordinateStart, that.coordinateEnd)
+                    && Arrays.equals(this.coordinateEnd, that.coordinateStart));
+    }
+}
